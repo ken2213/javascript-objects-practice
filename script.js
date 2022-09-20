11f3e8ae669163457481fc5aa812e929f251d3f8
Kenneth Ornieta <me@kennethornieta.com>
2022-09-20 10:40:56 +0800

    second commit

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ function Book(title, author, numOfPages, readOrNot) {
   this.numOfPages = numOfPages;
   this.readOrNot = readOrNot;
   this.describeBook = function() {
-    console.log(`Book Title: ${book1.title}\nAuthor: ${book1.title}\nNumber of Pages: ${book1.numOfPages}\nRead or Not? ${book1.readOrNot}`);
+    console.log(`Book Title: ${title}\nAuthor: ${title}\nNumber of Pages: ${numOfPages}\nRead or Not? ${readOrNot}`);
   }
 }
 
-const book1 = new Book('Peenoise', 'AKT', '200', true);
+const book1 = new Book('Peenoise', 'AKT', 200, true);
+const book2 = new Book('He\'s into her', 'Maxinejiji', 512, false);
 
-book1.describeBook()
+
+book1.describeBook();
+book2.describeBook();
